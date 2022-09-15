7356083f70789466e2b2e1ebd79202db903d08a2
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-09-15 20:30:22 +0900

    - first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y build-essential python-dev python-setuptools python-pip python-smbus
+sudo apt-get install -y libncursesw5-dev libgdbm-dev libc6-dev
+sudo apt-get install -y zlib1g-dev libsqlite3-dev tk-dev
+sudo apt-get install -y libssl-dev openssl
+sudo apt-get install -y libffi-dev
+
+wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+tar xvfz Python-3.7.9.tgz
