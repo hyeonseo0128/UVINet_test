79724b27274a1f51b5ad1ddeaef693b45e287c72
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-09-15 20:48:50 +0900

    - Add -y option

--- install_mavRouter.sh
+++ install_mavRouter.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/mavlink-router/mavlink-router.git
 cd mavlink-router
 git submodule update --init --recursive
-sudo apt install git meson ninja-build pkg-config gcc g++ systemd
+sudo apt install -y git meson ninja-build pkg-config gcc g++ systemd
 sudo pip3 install meson
 meson setup build .
 ninja -C build
