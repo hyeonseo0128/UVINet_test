98a21fb30027aedc8ca0d879664c5d7dfcfc8b5a
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-09-15 20:38:42 +0900

    - first commit

---
+++ install_mavRouter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+git clone https://github.com/mavlink-router/mavlink-router.git
+cd mavlink-router
+git submodule update --init --recursive
+sudo apt install git meson ninja-build pkg-config gcc g++ systemd
+sudo pip3 install meson
+meson setup build .
+ninja -C build
+sudo ninja -C build install
