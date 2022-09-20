b249c3252fdc7b1b2f7df347d3034a8c637918ca
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-09-20 23:30:18 +0900

    Create setupMavRouter.sh

---
+++ setupMavRouter.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+sudo gpasswd --add keti dialout
+sudo gpasswd --add keti tty
+sudo systemctl stop nvgetty.service
+sudo systemctl disable nvgetty.service
+
+sudo mkdir /etc/mavlink-router
+sudo cp main.conf /etc/mavlink-router
+
+cp runUDPMav.js
